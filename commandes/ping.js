18f3b4a5907ecc97a342798af2b972d93624c8db
Qartde <bonifacemd2525@gmail.com>
2026-03-09 00:53:58 +0300

    Add files via upload

---
+++ commandes/ping.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { zokou } = require("../framework/zokou");
+zokou({ nomCom: "ping", reaction: "☀", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = '𝐒𝐞𝐛𝐚 𝗿𝗲𝘀𝗽𝗼𝗻𝘀𝗲 𝘀𝗽𝗲𝗲𝗱 𝗶𝘀';
+    let d = '𝟖𝟎.𝟗𝟏𝐌/𝐬';
+    let varmess = z + d;
+    var img = 'https://files.catbox.moe/2yarwr.png';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+/*module.exports.commande = () => {
+  var nomCom = ["test","t"]
+  var reaction="☺️"
+  return { nomCom, execute,reaction }
+};
+
+async function  execute  (origineMessage,zok) {
+  console.log("Commande saisie !!!s")
+   let z ='Salut je m\'appelle *Zokou* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *Djalega++*'
+      let varmess=z+d
+      var img='https://telegra.ph/file/626e7105422c8908f723d.jpg'
+await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
+}  */ 
